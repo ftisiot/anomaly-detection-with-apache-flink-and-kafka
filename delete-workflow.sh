6d819efcb256986a7b0b4ebc5c66bf6e6a6a8fa4
Francesco <ftisiot@aiven.io>
2023-11-07 14:04:08 +0100

    added token

--- delete-workflow.sh
+++ delete-workflow.sh
@@ -1,3 +1,6 @@
-avn service terminate demo-kafka --force
-avn service terminate demo-flink --force
+# Load parameters (you need to create a params.conf file as copy of params.conf.sample and fill in the details)
+. ./fakedatagen/params.conf
+
+avn --auth-token $TOKEN service terminate demo-kafka --force
+avn --auth-token $TOKEN service terminate demo-flink --force
 docker stop $(docker ps -a -q --filter ancestor=fake-data-producer-for-apache-kafka-docker --format="{{.ID}}")
